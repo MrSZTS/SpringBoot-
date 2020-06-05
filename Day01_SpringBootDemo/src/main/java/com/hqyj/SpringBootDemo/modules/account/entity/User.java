f3520d1b9b31f98c169ff7f764f1baa9b5c58e7d
MrSZTS <983417768@qq.com>
2020-06-05 18:38:54 +0800

    "Jpa/自动生成表功能"

---
+++ Day01_SpringBootDemo/src/main/java/com/hqyj/SpringBootDemo/modules/account/entity/User.java
@@ -0,0 +1,84 @@
+package com.hqyj.SpringBootDemo.modules.account.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+/**
+ * 用户类
+ * 
+ * @author: HymanHu
+ * @date: 2019年11月28日
+ */
+//该类是个实体bean
+@Entity
+//数据库生成的表名
+@Table(name="user")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@Id	@GeneratedValue 为一个实体生成一个唯一标识的主键
+	private int userId;
+	private String userName;
+	private String password;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private Date createDate;
+	
+	@Transient
+	//@Transient	就是在给某个javabean上需要添加个属性，但是这个属性你又不希望给存到数据库中去，仅仅是做个临时变量，用一下。
+	private boolean rememberMe;
+	@Transient
+	private List<Role> roles;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public boolean getRememberMe() {
+		return rememberMe;
+	}
+
+	public void setRememberMe(boolean rememberMe) {
+		this.rememberMe = rememberMe;
+	}
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+}
