b42f3cf837e51d79085e98879906b68ab3eb1dd0
MrSZTS <983417768@qq.com>
2020-06-08 13:46:37 +0800

    "引入thymeleaf、页面的模块化、碎片化、页面数据处理"

--- Day01_SpringBootDemo/src/main/java/com/hqyj/SpringBootDemo/modules/test/controller/TestController.java
+++ Day01_SpringBootDemo/src/main/java/com/hqyj/SpringBootDemo/modules/test/controller/TestController.java
@@ -1,13 +1,21 @@
 package com.hqyj.SpringBootDemo.modules.test.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hqyj.SpringBootDemo.modules.test.entity.City;
+import com.hqyj.SpringBootDemo.modules.test.entity.Country;
+import com.hqyj.SpringBootDemo.modules.test.service.CityService;
+import com.hqyj.SpringBootDemo.modules.test.service.CountryService;
 import com.hqyj.SpringBootDemo.modules.test.vo.ApplicationTest;
 
 @Controller
@@ -27,6 +35,11 @@ public class TestController {
 	
 	@Autowired
 	private ApplicationTest applicationTest;
+	@Autowired
+	private CityService cityService;
+	@Autowired
+	private CountryService countryService;
+	
 	
 	private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 	/**
@@ -76,6 +89,36 @@ public class TestController {
 	//返回的是一个接口，而不是一个页面控制器
 	public String testDesc() {
 		return "This is test module desc";
+	}
+	
+	/**
+	 * 127.0.0.1/test/index
+	 * @return
+	 */
+	@RequestMapping("/index")
+	public String indexPage(ModelMap modelMap) {
+		
+		int countryId = 522;
+		List<City> cities = cityService.getCitiesByCountryId(countryId);
+		cities = cities.stream().limit(10).collect(Collectors.toList());
+		
+		Country country = countryService.getCountryByCountryId(countryId);
+		
+		modelMap.addAttribute("thymeleafTitle","王五");
+		modelMap.addAttribute("checked",true);
+		modelMap.addAttribute("currentNumber","99");
+		modelMap.addAttribute("changeType","checkbox");
+		modelMap.addAttribute("baiduUrl","/test/log");
+		modelMap.addAttribute("city",cities.get(0));
+		modelMap.addAttribute("shopLogo",
+				"https://static.veer.com/veer/static/resources/keyword/2020-02-19/533ed30de651499da1c463bca44b6d60.jpg");
+		modelMap.addAttribute("country",country);
+		modelMap.addAttribute("cities",cities);
+		modelMap.addAttribute("updateCityUri","/api/city");
+		
+		
+		modelMap.addAttribute("template","test/index");
+		return "index";//indexSimple
 	}
 	
 }
